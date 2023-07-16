488006d9c83e1649dcb5efd3bd2e2e2a86733345
teemoyangyyq <33918440+teemoyangyyq@users.noreply.github.com>
2023-07-16 18:31:41 +0800

    Update context.go

--- context.go
+++ context.go
@@ -4,15 +4,37 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net"
 	"os"
 	"path/filepath"
+	"strings"
+	"sync"
+	"time"
 
 	"mime/multipart"
 	"net/http"
+	"net/url"
+
+	"github.com/google/uuid"
 )
 
 var DefaultMultipartMemory int64
-
+var ServerIP = localIP()
+func localIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
 // 32 MB
 // 上下文
 type Context struct {
@@ -22,6 +44,46 @@ type Context struct {
 	HandlerSlice  []Handler              // 中间件，控制器方法数组
 	Index         int                    // 指定路由的当前执行的方法索引
 	StatusCode    int                    // 错误码
+	Errors        error
+	Size          int
+	sameSite      http.SameSite
+	Keys         map[string]any
+	mu           sync.RWMutex
+}
+var (
+	TraceIDKey = "x-trace-id"
+	StartTimeKey = "x-start-time"
+)
+func WithTraceID(ctx *Context) string {
+	traceID := GetTraceID(ctx)
+	if len(traceID) > 0 {
+		return traceID
+	}
+	traceID = NewTraceID()
+	
+	ctx.Res.Header().Set(TraceIDKey, traceID)
+	ctx.Set(TraceIDKey, traceID)
+	return traceID
+}
+
+// WithTraceID 注入 trace_id
+func WithStartTime(ctx *Context, startTime time.Time) {
+	ctx.Set(StartTimeKey, startTime)
+	return 
+}
+func (c *Context) Set(key string, value any) {
+	c.mu.Lock()
+	if c.Keys == nil {
+		c.Keys = make(map[string]any)
+	}
+
+	c.Keys[key] = value
+	c.mu.Unlock()
+}
+
+// 进入对应路由的下一个方法
+func (c *Context) Abort() {
+	c.Index = 100
 }
 
 // 进入对应路由的下一个方法
@@ -34,9 +96,69 @@ func (c *Context) Next() {
 
 }
 
+func (c *Context) ClientIp() string{
+	ip := ClientPublicIP(c.Req)
+	if ip == ""{
+	ip = ClientIP(c.Req)
+	}
+	return ip
+
+}
+func ClientPublicIP(r *http.Request) string {
+	var ip string
+	for _, ip = range strings.Split(r.Header.Get("X-Forwarded-For"), ",") {
+		ip = strings.TrimSpace(ip)
+		if ip != ""  {
+			return ip
+		}
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != ""  {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+	
+		return ip
+
+	}
+	return ""
+	}
+
+
+func ClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+	return ip
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+	return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+	return ip
+	}
+	return ""
+	}
+
+
 func (c *Context) PostForm(key string) string {
 	return c.Req.FormValue(key)
 }
+
+// GetTraceID 获取用户请求标识
+func GetTraceID(ctx *Context) string {
+	id, _ := ctx.Get(TraceIDKey)
+	return id.(string)
+}
+
+func (c *Context) Get(key string) (value any, exists bool) {
+	c.mu.RLock()
+	value, exists = c.Keys[key]
+	c.mu.RUnlock()
+	return
+}
+
 
 func (c *Context) Query(key string) string {
 	return c.Req.URL.Query().Get(key)
@@ -48,15 +170,7 @@ func (c *Context) PathParam(key string) interface{} {
 	}
 	return nil
 }
-func (c *Context) PathParamInt(key string) int {
-	return c.PathParam(key).(int)
-}
-func (c *Context) PathParamInt64(key string) int64 {
-	return c.PathParam(key).(int64)
-}
-func (c *Context) PathParamString(key string) string {
-	return c.PathParam(key).(string)
-}
+
 func (c *Context) Status(code int) {
 	c.StatusCode = code
 	c.Res.WriteHeader(code)
@@ -69,29 +183,113 @@ func (c *Context) SetHeader(key string, value string) {
 func (c *Context) String(code int, format string, values ...interface{}) {
 	c.SetHeader("Content-Type", "text/plain")
 	c.Status(code)
-	c.Res.Write([]byte(fmt.Sprintf(format, values...)))
+	n, err := c.Res.Write([]byte(fmt.Sprintf(format, values...)))
+	if err != nil {
+		c.Errors = err
+	}
+	c.Size += n			
 }
-
-
 
 func (c *Context) JSON(code int, obj interface{}) {
 	c.SetHeader("Content-Type", "application/json")
 	c.Status(code)
-	encoder := json.NewEncoder(c.Res)
-	if err := encoder.Encode(obj); err != nil {
+	resbyte , err := json.Marshal(obj)
+	if err != nil {
 		http.Error(c.Res, err.Error(), 500)
+	} 
+	c.Size += len(resbyte)
+	c.Res.Write(resbyte)
+    
+	
+		
+}
+
+// GetRawData returns stream data.
+func (c *Context) GetRawData() ([]byte, error) {
+	return ioutil.ReadAll(c.Req.Body)
+}
+
+// SetSameSite with cookie
+func (c *Context) SetSameSite(samesite http.SameSite) {
+	c.sameSite = samesite
+}
+
+func (c *Context) GetKeys(key string) (value any, exists bool) {
+	c.mu.RLock()
+	value, exists = c.Keys[key]
+	c.mu.RUnlock()
+	return
+}
+// SetCookie adds a Set-Cookie header to the ResponseWriter's headers.
+// The provided cookie must have a valid Name. Invalid cookies may be
+// silently dropped.
+func (c *Context) SetCookie(name, value string, maxAge int, path, domain string, secure, httpOnly bool) {
+	if path == "" {
+		path = "/"
 	}
+	http.SetCookie(c.Res, &http.Cookie{
+		Name:     name,
+		Value:    url.QueryEscape(value),
+		MaxAge:   maxAge,
+		Path:     path,
+		Domain:   domain,
+		SameSite: c.sameSite,
+		Secure:   secure,
+		HttpOnly: httpOnly,
+	})
+}
+
+// Cookie returns the named cookie provided in the request or
+// ErrNoCookie if not found. And return the named cookie is unescaped.
+// If multiple cookies match the given name, only one cookie will
+// be returned.
+func (c *Context) Cookie(name string) (string, error) {
+	cookie, err := c.Req.Cookie(name)
+	if err != nil {
+		return "", err
+	}
+	val, _ := url.QueryUnescape(cookie.Value)
+	return val, nil
+}
+
+func NewTraceID() string {
+	return uuid.New().String()
+}
+
+func (c *Context) PathParamInt(key string) int {
+	v :=  c.PathParam(key)
+	if v == nil {
+		return 0
+	}
+	return v.(int)
+}
+func (c *Context) PathParamInt64(key string) int64 {
+	v :=  c.PathParam(key)
+	if v == nil {
+		return 0
+	}
+	return v.(int64)
+}
+func (c *Context) PathParamString(key string) string {
+	v :=  c.PathParam(key)
+	if v == nil {
+		return ""
+	}
+	return v.(string)
 }
 
 func (c *Context) Data(code int, data []byte) {
 	c.Status(code)
+	c.Size += len(data)
 	c.Res.Write(data)
 }
 
 func (c *Context) HTML(code int, html string) {
 	c.SetHeader("Content-Type", "text/html")
 	c.Status(code)
-	c.Res.Write([]byte(html))
+	resbyte := []byte(html)
+	c.Size += len(resbyte)
+	c.Res.Write(resbyte)
 }
 
 func (c *Context) FormFile(name string) (*multipart.FileHeader, error) {
